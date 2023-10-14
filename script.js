196ad3bfc06558c5ed5af5c1b445c0a939b52860
Lenal-P <lenalpiggy@gmail.com>
2023-10-14 13:23:21 +0700

    Slideshow

--- script.js
+++ script.js
@@ -22,3 +22,38 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+
+/* ---------------------- BEGIN SLIDESHOW ---------------------- */
+document.addEventListener('DOMContentLoaded', function() {
+    const listImages = document.querySelector('.list_images');
+    const prevBtn = document.querySelector('.control.prev');
+    const nextBtn = document.querySelector('.control.next');
+    let currentIndex = 0;
+
+    function showSlide(index) {
+        listImages.style.transform = `translateX(-${index * 100}%)`;
+        currentIndex = index;
+    }
+
+    function showNextSlide() {
+        if (currentIndex < listImages.children.length - 1) {
+            showSlide(currentIndex + 1);
+        } else {
+            showSlide(0);
+        }
+    }
+
+    function showPrevSlide() {
+        if (currentIndex > 0) {
+            showSlide(currentIndex - 1);
+        } else {
+            showSlide(listImages.children.length - 1);
+        }
+    }
+
+    nextBtn.addEventListener('click', showNextSlide);
+    prevBtn.addEventListener('click', showPrevSlide);
+
+    setInterval(showNextSlide, 5000);
+});
+/* ---------------------- END SLIDESHOW ---------------------- */
